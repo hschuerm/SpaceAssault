d7a193d2337c8b67246fb90e044f864fb6890e19
Hendrik Schürmann <>
2022-03-21 11:07:28 +0100

    ADDED webserver

--- server/src/setupServer.js
+++ server/src/setupServer.js
@@ -1,13 +1,25 @@
+const path = require('path');
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 
-const app = express();
-const httpServer = http.createServer(app);
-
 const Config = require('./loader/config');
 
 module.exports = () => {
+    const app = express();
+
+    const clientPath = __dirname + '/../../client/build/';
+    app.use(express.static(clientPath));
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(clientPath), function (err) {
+            if (err) {
+                res.status(500).send(err)
+            }
+        })
+    });
+
+    const httpServer = http.createServer(app);
+
     const socketServer = new Server(httpServer, Config.websocket);
 
     socketServer.on('connection', (socket) => {
